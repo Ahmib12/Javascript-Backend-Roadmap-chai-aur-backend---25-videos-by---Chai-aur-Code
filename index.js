55307c2edcf780500d393cd829b4b3e62c46e5f4
syedatasmiya <syedtasmiya@globussoft.in>
2025-08-08 12:56:44 +0530

    github data added

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const express = require("express"); //  u can use any of these (this and below) 
 const app = express(); // app is very powerful here, u can use many things here , (its like some library -- eg:- math, wiht that u can use amny stuffs - pi, swrt,cube wtc)
 const port = 3000; // u can use anything
 
+const githubData = {
+  "id": 1,
+  "firstName": "Emily",
+  "lastName": "Johnson",
+  "maidenName": "Smith",
+  "age": 28,
+  "gender": "female",
+  "email": "emily.johnson@x.dummyjson.com",
+  "phone": "+81 965-431-3024",
+  "username": "emilys",
+  "password": "emilyspass",
+  "birthDate": "1996-5-30",
+  "image": "https://dummyjson.com/icon/emilys/128",
+  "bloodGroup": "O-",
+  "height": 193.24,
+  "weight": 63.16,
+  "eyeColor": "Green",
+  }
+
 app.get("/", (req, res) => {
   res.send("Hello World!"); // if u get any request in the / then give the below response..
 });
@@ -21,6 +40,11 @@ app.get("/movie",(req,res)=>{
 app.get("/youtube",(req,res)=>{--
   res.send("<h2>Youtube...</h2>")
 })
+
+app.get('/github',(req,res) => {
+  res.json(githubData)
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
