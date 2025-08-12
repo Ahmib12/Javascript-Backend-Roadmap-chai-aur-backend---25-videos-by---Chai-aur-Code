f6c570bec67427190a0c18544360f6dfff7c5458
syedatasmiya <syedtasmiya@globussoft.in>
2025-08-12 18:56:44 +0530

    learnt about the FS app - backend and frontend

---
+++ 31FullStackFEAndBE/Backend/server.js
@@ -0,0 +1,43 @@
+// require('dotenv').config(); 
+
+// 1 - import express
+// const express = require('express') // - this  is the old code -- these days we use import
+import express from "express" 
+
+// 2 - create app
+const app = express();
+
+// 3 -  app work is to navigate to some route
+app.get('/',(req,res)=>{
+    res.send('server is ready')
+})
+
+// get a list of 5 jokes
+// app.get('/jokes',(req,res)=>{ // we dont directly wrote /jokes -- we need to use /api/v1/jokes or api/jokes 
+app.get('/api/jokes',(req,res)=>{
+    const jokes = [
+      {
+        id: 1,
+        title: "A joke 1",
+        content: "this is a joke 1",
+      },
+      {
+        id: 2,
+        title: "A joke 2",
+        content: "this is a joke 2",
+      },
+      {
+        id: 3,
+        title: "A joke 3",
+        content: "this is a joke 3",
+      },
+    ];
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 5000; // if u push ur code to production ,then 100% it shd run from the env only, else ur code or files wil not run
+// whether its in aws, or 
+
+app.listen(port,() =>{
+    console.log(`server listening on port -- http://localhost:${port}`);
+})
---
+++ 31FullStackFEAndBE/Frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes,setJokes] = useState([]);
+
+  useEffect(() =>{
+    axios.get("http://localhost:5000/jokes")
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch((error) =>{
+      console.log(error);
+    })
+  })
+
+  return (
+    <>
+      <h1>Syeda's world</h1>
+      <p>JOKES : {jokes.length}</p>
+      {
+        jokes.map((joke,index)=>{
+          <div key={joke.id}>
+            <h3> TITLE :- {joke.title}</h3>  
+            <h3> Content:- {joke.content}</h3>  
+          </div>
+        })
+      }
+    </>
+  )
+}
+
+export default App
